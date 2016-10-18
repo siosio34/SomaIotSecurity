e9e2334c20ed8589d4152eaf0bd8ddcef54a20f5
young <siosio34@nate.com>
2016-10-18 21:37:05 +0900

    make login sample test code

--- temp/app/controllers/login_controller.rb
+++ temp/app/controllers/login_controller.rb
@@ -3,4 +3,22 @@ class LoginController < ApplicationController
   def index
 
   end
+
+  def auth
+    @pass = params[:password]
+    passjson = {:page_name => 'login', :admin_pw => @pass}
+    client.puts JSON.generate(passjson)
+
+    while (line = client.gets)
+      my_hash = JSON.parse(line)
+      if my_hash[verify] == "true"
+        render 'welcome/index'
+      else
+        render 'login/index'
+      end
+    end
+    client.close
+  end
+
+
 end
