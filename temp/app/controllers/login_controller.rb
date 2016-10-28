f266597fb0ad7bd3ba477e15fd02a33beddf2bb8
young <siosio34@nate.com>
2016-10-28 18:43:15 +0900

    my commit

--- temp/app/controllers/login_controller.rb
+++ temp/app/controllers/login_controller.rb
@@ -1,3 +1,6 @@
+require 'socket'
+require 'json'
+
 class LoginController < ApplicationController
 
   def index
@@ -7,10 +10,11 @@ class LoginController < ApplicationController
   def auth
     @pass = params[:password]
     passjson = {:page_name => 'login', :admin_pw => @pass}
+    client = TCPSocket.new 'localhost', 9990
     client.puts JSON.generate(passjson)
 
     while (line = client.gets)
-      my_hash = JSON.parse(line)
+      my_hash = JSON.parse(line, :quirks_mode => true)
       if my_hash[verify] == "true"
         render 'welcome/index'
       else
