c786c8343ef2c919b869481342a1a8fbd03ca179
westwoods <kdw8681@naver.com>
2016-10-28 19:22:17 +0900

    구조체에 iw divice 내용을 입력

--- src/state_mgr.h
+++ src/state_mgr.h
@@ -1,6 +1,6 @@
 #define STATE_TYPE_NUM 14
 
-struct conndiv_state {
+typedef struct{
 	char station[20];
 	char inactivetime[20];
 	char rxbytes[20];
@@ -15,7 +15,7 @@ struct conndiv_state {
 	char authenticated[20];
 	char WMMWME[20];
 	char TDLSpeer[20];
-};
+}div_state;
 char *text_t[] = {
 	"Station",
 	"inactivetime:",
@@ -32,16 +32,26 @@ char *text_t[] = {
 	"WMM/WME:",
 	"TDLSpeer:"
 };
-char* retText(char* arg) {
+
+char* retText(char *arg,div_state *div) {
 	int i = 0;
 	char *ptr;
-	for (i = 0; i < 
-STATE_TYPE_NUM; i++)
+	for (i = 0; i <STATE_TYPE_NUM; i++)
 	{
-		if ((ptr = 
-strstr(arg, text_t[i]))) {
-			printf("%s\n", 
-ptr += strlen(text_t[i]));
+		if ((ptr =strstr(arg, text_t[i]))) {
+			printf("%s", ptr += strlen(text_t[i]));
+			switch(i)
+			{
+				case 0: sprintf((*div).station,"%s",ptr); break;
+				case 1:	sprintf((*div).inactivetime,"%s",ptr); break;
+                                case 2:
+                                case 3:
+                                case 4:
+                                case 5:
+                                case 6:
+                                case 7:
+				default : break;
+			}
 			break;
 		}
 	}
